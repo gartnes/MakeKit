927f242b7e0061abef606b9d8995de9fecb6d41b
Simen Rognli Mikalsen <smi052@post.uit.no>
2022-02-24 11:50:44 +0100

    La til navigation til Air:Bit

--- app/src/main/java/com/example/makekit/MainScreen.java
+++ app/src/main/java/com/example/makekit/MainScreen.java
@@ -4,15 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
+        setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
     }
 }
--- app/src/main/java/com/example/makekit/StartScreen.java
+++ app/src/main/java/com/example/makekit/StartScreen.java
@@ -2,7 +2,11 @@ package com.example.makekit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        Button btn_airbit = findViewById(R.id.btn_airbit);
+
+        btn_airbit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartScreen.this, MainScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
