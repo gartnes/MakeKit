381199960da730532b1a8e5c56f250b62767824f
gartnes <tan092@uit.no>
2022-02-24 11:43:52 +0100

    Lagt til knapper på StartScreen

--- app/src/main/java/com/example/makekit/MainActivity.java
+++ app/src/main/java/com/example/makekit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
     }
