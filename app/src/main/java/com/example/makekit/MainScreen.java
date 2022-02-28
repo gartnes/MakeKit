a22413d64ec32c62aa6ec5d600252d5910181e4e
Simen Rognli Mikalsen <smi052@post.uit.no>
2022-02-28 13:58:00 +0100

    La til kode for layouts, samt bilder for knappene

--- app/src/main/java/com/example/makekit/MainScreen.java
+++ app/src/main/java/com/example/makekit/MainScreen.java
@@ -7,14 +7,16 @@ import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainScreen extends AppCompatActivity {
 
-    Button segment_hoverbit, segment_airbit, btn_start,
-            throttle_up, throttle_down, yaw_left, yaw_right;
+    ImageButton btn_yaw_left, btn_yaw_right,
+            btn_pitch_forward, btn_pitch_backwards;
+    Button btn_throttle_up, btn_throttle_down, btn_start, segment_hoverbit, segment_airbit;
 
     int throttle;
 
@@ -28,15 +30,20 @@ public class MainScreen extends AppCompatActivity {
         segment_hoverbit = findViewById(R.id.segment_hoverbit);
         segment_airbit = findViewById(R.id.segment_airbit);
         btn_start = findViewById(R.id.btn_start);
-        throttle_up = findViewById(R.id.btn_throttle_up);
-        throttle_down = findViewById(R.id.btn_throttle_down);
+        btn_throttle_up = findViewById(R.id.btn_throttle_up);
+        btn_throttle_down = findViewById(R.id.btn_throttle_down);
+        btn_pitch_forward = findViewById(R.id.btn_pitch_forward);
+        btn_pitch_backwards = findViewById(R.id.btn_pitch_backwards);
 
+        //Layouts
         LinearLayout buttons_yaw = findViewById(R.id.linlay_yaw);
 
         segment_hoverbit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 buttons_yaw.setVisibility(View.INVISIBLE);
+                btn_pitch_forward.setVisibility(View.INVISIBLE);
+                btn_pitch_backwards.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -44,6 +51,8 @@ public class MainScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 buttons_yaw.setVisibility(View.VISIBLE);
+                btn_pitch_forward.setVisibility(View.VISIBLE);
+                btn_pitch_backwards.setVisibility(View.VISIBLE);
             }
         });
 
@@ -55,13 +64,13 @@ public class MainScreen extends AppCompatActivity {
             }
         });
 
-        throttle_up.setOnClickListener(new View.OnClickListener() {
+        btn_throttle_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 throttle += 1;
             }
         });
-        throttle_down.setOnClickListener(new View.OnClickListener() {
+        btn_throttle_down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (throttle > 0){
