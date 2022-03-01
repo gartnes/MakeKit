9fc5ee8d497a4271fe549233e63b15859cacdf10
gartnes <tan092@uit.no>
2022-03-01 15:13:24 +0100

    Fjernet if test i BleHardwareScanner

--- app/src/main/java/com/example/makekit/ble/BleHardwareScanner.java
+++ app/src/main/java/com/example/makekit/ble/BleHardwareScanner.java
@@ -5,9 +5,8 @@ import android.os.Build;
 
 public class BleHardwareScanner {
     public static BleScanner getBleScanner(Context context) {
-        if (Build.VERSION.SDK_INT >= 21) {
-            return new BleScannerAndroid5plus(context);
-        }
-        return new BleScannerAndroid4(context);
+
+        return new BleScannerAndroid5plus(context);
+
     }
 }
